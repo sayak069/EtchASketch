a50cf3e36c98172ab542ea8c1f11c98e1a1ded6e
sayak069 <sayakbasak720@gmail.com>
2023-09-14 21:08:02 +0530

    added ui and color effect

---
+++ script.js
@@ -0,0 +1,56 @@
+// dynamic divs creation
+const container = document.getElementById('grid-box');
+const gridSizeInput = document.getElementById('gridSize');
+const createGridButton = document.getElementById('createGrid');
+
+createGrid()
+createGridButton.addEventListener('click', createGrid);
+function createGrid() {
+    const gridSize = parseInt(gridSizeInput.value);
+
+    if (!isNaN(gridSize)) {
+        container.setAttribute('style', `grid-template-columns:repeat(${gridSize},1fr); grid-template-rows:repeat(${gridSize},1fr)`)
+        // Clear existing grid items
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+        // Create and append new grid items
+        for (let i = 0; i < gridSize * gridSize; i++) {
+            const gridItem = document.createElement('div');
+            gridItem.setAttribute('style', `height:${49 / gridSize}vw;width:${49 / gridSize}vw`)
+            gridItem.classList.add('grid-item');
+            container.appendChild(gridItem);
+        }
+    }
+}
+// live value of slider
+const span = document.getElementById('value')
+gridSizeInput.addEventListener('input', () => {
+    span.textContent = gridSizeInput.value
+})
+// Change the color of all dynamically added divs
+container.addEventListener('click', changeColor)
+function changeColor() {
+    const dynamicDivs = document.querySelectorAll('#grid-box > div')
+    dynamicDivs.forEach((div) => {
+        div.addEventListener('mouseenter', () => {
+            div.setAttribute('style', `background:${getRandomColor()}`)
+        })
+    })
+}
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// clear divs
+const clear=document.getElementById('clear')
+clear.addEventListener('click',()=>{
+    document.querySelectorAll('#grid-box > div').forEach(div=>{
+        div.setAttribute('style','background:white')
+    })
+})
